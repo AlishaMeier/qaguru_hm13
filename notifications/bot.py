3ba6ab89b11dcc115b96a594f2f1d80d13a5bdc4
AlishaMeier <alisha.meyer@gmail.com>
2025-09-21 01:15:33 +0500

    changed bot.py

--- notifications/bot.py
+++ notifications/bot.py
@@ -1,22 +1,25 @@
 import os
 import json
+import asyncio
 from dotenv import load_dotenv
 from telegram import Bot
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CONFIG_PATH = os.path.join(BASE_DIR, "config.json")
+
+# Загружаем .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT")
 
+if not TOKEN or not CHAT_ID:
+    raise ValueError("TOKEN or CHAT_ID not founded. Check .env!")
 
-with open("config.json", "r") as f:
-    config = json.load(f)
+bot = Bot(token=TOKEN)
 
+async def send_message():
+    await bot.send_message(chat_id=CHAT_ID, text="Finish! ✅")
+    print("Message sent")
 
-config["telegram"]["token"] = TOKEN
-config["telegram"]["chat"] = CHAT_ID
-
-
-bot = Bot(token=config["telegram"]["token"])
-bot.send_message(chat_id=config["telegram"]["chat"], text="Finish! ✅")
-
-print("Notification sent to TG")
+asyncio.run(send_message())
