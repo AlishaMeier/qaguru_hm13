507dff0cdd7644fe346382ff6f0c127003e61ae3
AlishaMeier <alisha.meyer@gmail.com>
2025-09-21 19:41:21 +0500

    added check form

--- conftest.py
+++ conftest.py
@@ -1,58 +1,70 @@
-import pytest
 import os
-from dotenv import load_dotenv
+import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene.support.shared import browser
+from dotenv import load_dotenv
 from utils import attach
 
+# Загружаем переменные окружения
+load_dotenv()
 
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-    global selenoid_login, selenoid_pass, selenoid_url, selenoid_video_url
-    selenoid_login = os.getenv("SELENOID_LOGIN", "")
-    selenoid_pass = os.getenv("SELENOID_PASS", "")
-    selenoid_url = os.getenv("SELENOID_URL")
-    selenoid_video_url = os.getenv("SELENOID_VIDEO_URL", "")
+SELENOID_URL = os.getenv("SELENOID_URL")
+SELENOID_LOGIN = os.getenv("SELENOID_LOGIN", "")
+SELENOID_PASS = os.getenv("SELENOID_PASS", "")
+SELENOID_VIDEO_URL = os.getenv("SELENOID_VIDEO_URL", "")
 
-    print("SELENOID_URL =", selenoid_url)
-    print("SELENOID_VIDEO_URL =", selenoid_video_url)
-    print("SELENOID_LOGIN =", "set" if selenoid_login else "empty")
-    print("SELENOID_PASS =", "set" if selenoid_pass else "empty")
+# Настройки браузера
+browser.config.base_url = "https://demoqa.com"
+browser.config.window_width = 1920
+browser.config.window_height = 1080
 
+# Хук для скриншотов в случае падения теста
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    if report.failed:
+        screenshot_path = 'failure.png'
+        browser.driver.save_screenshot(screenshot_path)  # <- исправлено
+        allure.attach.file(
+            screenshot_path,
+            name='failure',
+            attachment_type=allure.attachment_type.PNG
+        )
 
+# Фикстура для браузера
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
     options = Options()
-    caps = {
-        "browserName": "chrome",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(caps)
+    options.set_capability("browserName", "chrome")
+    options.set_capability("selenoid:options", {"enableVNC": True, "enableVideo": True})
 
+    if SELENOID_URL:
+        if SELENOID_LOGIN and SELENOID_PASS:
+            remote_url = f"http://{SELENOID_LOGIN}:{SELENOID_PASS}@{SELENOID_URL}/wd/hub"
+        else:
+            remote_url = f"http://{SELENOID_URL}/wd/hub"
 
-    if selenoid_login and selenoid_pass:
-        remote_url = f"http://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub"
+        driver = webdriver.Remote(
+            command_executor=remote_url,
+            options=options
+        )
+        browser.config.driver = driver
+
+        yield browser
+
+        # После теста добавляем артефакты
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
+        attach.add_video(browser)
+
+        driver.quit()
     else:
-        remote_url = f"http://{selenoid_url}/wd/hub"
-
-    driver = webdriver.Remote(
-        command_executor=remote_url,
-        options=options
-    )
-
-    browser.config.driver = driver
-
-    yield browser
-
-
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
-
-    driver.quit()
+        # Локальный запуск
+        driver = webdriver.Chrome(options=options)
+        browser.config.driver = driver
+        yield browser
+        driver.quit()
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,55 +1,61 @@
-from selene import browser, be, have
 import os
-from utils import attach
-image_path = os.path.abspath("../resources/test_check.txt")
 import allure
 from selene.support.shared import browser
+from selene import be, have
+from utils import attach
 
+IMAGE_PATH = os.path.join(os.path.dirname(__file__), 'resources/test_check.txt')
+
+@allure.title("Filled registration form")
 def test_fill_form(setup_browser):
-    browser = setup_browser
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.open("/automation-practice-form")
 
-    attach.add_screenshot(browser)
-    with allure.step("Fill name"):
+    with allure.step("Filled name and surname"):
         browser.element('[id="firstName"]').should(be.visible).type("Alisha")
-    with allure.step("Fill surname"):
         browser.element('[id="lastName"]').should(be.visible).type("Meier")
-    with allure.step("Fill email"):
+
+    with allure.step("Filled email"):
         browser.element('[id="userEmail"]').should(be.visible).type("alisha.meyerr@gmail.com")
+
     with allure.step("Choose gender"):
         browser.element('label[for="gender-radio-2"]').click()
-    with allure.step("Fill phone"):
+
+    with allure.step("Fillen phone number"):
         browser.element('[id="userNumber"]').should(be.visible).type("7078083369")
-    with allure.step("Choose BD"):
+
+    with allure.step("Choose DB"):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').type('November')
         browser.element('.react-datepicker__year-select').type('1995')
         browser.element('.react-datepicker__day--003').click()
+
     with allure.step("Choose subject"):
         browser.element('#subjectsInput').should(be.visible).type('Computer Science').press_enter()
-    with allure.step("Choose hobbies"):
+
+    with allure.step("Choose hobbie"):
         browser.element('label[for ="hobbies-checkbox-3"]').click()
+
     with allure.step("Upload image"):
-        browser.element('[id = "uploadPicture"]').should(be.visible).send_keys(image_path)
-    with allure.step("Fill address"):
+        browser.element('[id="uploadPicture"]').should(be.visible).send_keys(IMAGE_PATH)
+
+    with allure.step("Filled address"):
         browser.element('[id="currentAddress"]').should(be.visible).type("Almaty")
-    with allure.step("Choose state"):
+
+    with allure.step("Choose state and city"):
         browser.element('#state input').type('NCR').press_enter()
-    with allure.step("Choose city"):
-        browser.element('[id="city"]').should(be.visible).click()
-        browser.element('[id="react-select-4-input"]').type('Delhi').press_enter()
+        browser.element('#city input').type('Delhi').press_enter()
+
     with allure.step("Send form"):
         browser.element('#submit').click()
-    with allure.step("Check sending form"):
+
+    with allure.step("Check success sent"):
         browser.element('[id="example-modal-sizes-title-lg"]').should(be.visible).should(have.text('Thanks for submitting the form'))
 
-def test_successfully_filling(setup_browser):
     with allure.step("Check form"):
-        test_fill_form(setup_browser)
-        attach.add_screenshot(browser)
         table_element = browser.all('table.table-dark tbody tr')
         table_element.element_by(have.text('Student Name')).all('td').second.should(have.text('Alisha Meier'))
-        table_element.element_by(have.text('Student Email')).all('td').second.should(have.text('alisha.meyerr@gmail.com'))
+        table_element.element_by(have.text('Student Email')).all('td').second.should(
+            have.text('alisha.meyerr@gmail.com'))
         table_element.element_by(have.text('Gender')).all('td').second.should(have.text('Female'))
         table_element.element_by(have.text('Mobile')).all('td').second.should(have.text('7078083369'))
         table_element.element_by(have.text('Date of Birth')).all('td').second.should(have.text('03 November,1995'))
